4ab2785cf17499658e4885922f978022a6cb0479
javasusanto <heri.susanto@loratechai.com>
2023-09-10 16:34:46 +0700

    React extensions

---
+++ src/options/options.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './options.css';
+
+const App: React.FC<{}> = () => {
+  return (
+    <div>
+      <img src='icon.png' alt='icon' />
+    </div>
+  );
+};
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+render(<App />, root);
